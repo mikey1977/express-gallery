9948b3117b62ce997f573c9e304dfe6dc4e443c6
mikey1977 <mmi@umich.edu>
2015-10-19 18:10:33 -1000

    css link added to mainLayout.jade

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-  res.send('All users');
+  res.send('view a list of gallery photos');
+});
+
+router.get('/:id', function (req, res) {
+  res.send('see a single gallery photo');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,17 +2,22 @@ var express = require('express');
 var app = express();
 var users = require('./routes/users');
 var jade = require('jade');
+
 app.set('view engine', 'jade');
 app.set('views', './views');
 
+// all static files check from here
+app.use(express.static('public'));
+
+app.use('/users', users);
+
 app.get('/', function (req, res) {
-  // res.send('Are we working?');
+
   res.render('index', {
     title : '_ARCHITEKT'
   });
 });
 
-app.use('/users', users);
 
 var server = app.listen(3000, function() {
   var host = server.address().address;
